a1c9ace26cdb1d0cb66eaeac128dc495d4e4af42
ergashev <akbar.ergashev@outlook.com>
2025-01-24 21:05:56 +0500

    remove elm

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ namespace LeetCoding
     {
         private static void Main(string[] args)
         {
-            int[] nums1 = [0, 1, 2, 2, 3, 0, 4, 2];
-            int val = 2;
+            int[] nums1 = [3, 2, 2, 3];
+            int val = 3;
 
-
+            var ni = nums1.Length;
             for (int i = 0; i < nums1.Length; i++)
             {
-                if (nums1[i] == val)
-                {
-                    
-                }
+                if (nums1[i] != val) continue;
+                while (ni > -1 && nums1[--ni] == val) { }
+                if (i + ni == nums1.Length) {} // return nums1, i
+                //swap
             }
         }
     }
